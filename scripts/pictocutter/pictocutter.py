ccde8be119d6cb8985f28dcc810ac2cb8a09260a
yunyl <32681723+heyimyunyl@users.noreply.github.com>
2021-08-16 18:42:15 +0300

    fix pictocutter

--- scripts/pictocutter/pictocutter.py
+++ scripts/pictocutter/pictocutter.py
@@ -1,43 +1,31 @@
-from __future__ import print_function
+import sys, os, urllib.request
 from PIL import Image
-import os, sys
-import urllib.request
+from io import BytesIO
 
+def PictoCutter(MapName = (sys.argv[1:])[0], CoachCount = (sys.argv[1:])[1], ResizeAgain=True):
+    os.makedirs("scripts/pictocutter/picto_output/" + MapName, exist_ok=True)
+    
+    css = urllib.request.urlopen("https://jdnowweb-s.cdn.ubi.com/uat/release_tu2/20150928_1740/songs/" + MapName + "/assets/web/pictos-sprite.css").read().decode("utf8").split("\n")
+    atlas = Image.open(BytesIO(urllib.request.urlopen("https://jdnowweb-s.cdn.ubi.com/uat/release_tu2/20150928_1740/songs/" + MapName + "/assets/web/pictos-sprite.png").read()))
+    
+    if int(CoachCount) > 1:
+        y1 = 40
+        x1 = 217
+    else:
+        y1 = 0
+        x1 = 256
+    x = 256
+    y = 0
+    for picto in css:
+        PictoName = picto.split("-")
+        PictoName = PictoName[1].split("{")
+        PictoName = PictoName[0]
+        picto = atlas.crop((y,y1,x,x1))
+        y = y + 256
+        x = x + 256
+        if (int(CoachCount) > 1) and ResizeAgain:
+            picto = picto.resize((256,  256))
+        picto.save("./scripts/pictocutter/picto_output/"+ MapName + "/" + PictoName + ".png")
+    print("done")
 
-os.makedirs('scripts/pictocutter/picto_input', exist_ok=True)
-os.makedirs('scripts/pictocutter/picto_output', exist_ok=True)
-codename = (sys.argv[1:])[0]
-os.mkdir("scripts/pictocutter/picto_output/" + codename )
-
-urlpng = "http://jdnowweb-s.cdn.ubi.com/uat/release_tu2/20150928_1740/songs/" + codename + "/assets/web/pictos-sprite.png"
-urlcss = "http://jdnowweb-s.cdn.ubi.com/uat/release_tu2/20150928_1740/songs/" + codename + "/assets/web/pictos-sprite.css"
-urllib.request.urlretrieve(urlpng, "scripts/pictocutter/picto_input/pictos-sprite.png")
-urllib.request.urlretrieve(urlcss, "scripts/pictocutter/picto_input/pictos-sprite.css")
-
-pictosspritepng = Image.open(f'scripts/pictocutter/picto_input/pictos-sprite.png')
-
-a = 7
-b = 0
-c = 0
-d = 256
-
-howmanypictos = (pictosspritepng.size[0] // 256)
-
-for line in open(f'scripts/pictocutter/picto_input/pictos-sprite.css'):
-    while ((line[(b - 1):b]) == "{") == False:
-        b+=1
-    if ((line[(b - 1):b]) == "{") == True:
-        b-=1
-        pictoname = (line[a:b])
-        b = 0
-        width, height = pictosspritepng.size # Get dimensions 
-        left = c
-        top = height - 256
-        right = d
-        bottom = 2 * height - 256
-        cropped_example = pictosspritepng.crop((left, top, right, bottom))
-        cropped_example.save("./scripts/pictocutter/picto_output/"+ codename + "/" + pictoname + ".png")
-        c+=256
-        d+=256
-	
-print("done")
+PictoCutter()
